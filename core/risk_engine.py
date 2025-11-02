b42cdc0ce9517c8417cf5b7e460b601fdf5450c3
sneh01 <sp3568@drexel.edu>
2025-11-01 21:16:22 -0400

    added position size calc

--- core/risk_engine.py
+++ core/risk_engine.py
@@ -6,3 +6,10 @@ class RiskEngine:
 
     def validate_trade(self, trade):
         return True  # Simplified placeholder
+    
+    def calculate_position_size(self, account_balance, stop_loss_ticks):
+        risk_amount = account_balance * self.config.risk_per_trade
+        tick_value = 12.5
+        stop_loss_value = stop_loss_ticks * tick_value
+        size = risk_amount / stop_loss_value
+        return max(1, int(size))
